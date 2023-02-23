0f73b515112a7de623f735ca9fd5db3e8a88c534
Paola Perez Valencia <paola.pv1999@gmail.com>
2023-02-23 14:39:41 -0600

    UPDATE Other headers working property

--- src/components/HeadingSection/HeadingSection.js
+++ src/components/HeadingSection/HeadingSection.js
@@ -7,28 +7,18 @@ const HeadingSection = ({ title, tagline, classAppend, font, children }) => {
     >
       <h2
         className={font ? font : ""}
-        data-aos-delay={100}
-        data-aos={"fade-up"}
-        data-aos-duration={700}
       >
         {title}
       </h2>
       {tagline && (
         <h4
           className={"text-uppercase " + (font ? font : "")}
-          data-aos-delay={200}
-          data-aos={"fade-up"}
-          data-aos-duration={700}
         >
           {`- ${tagline} -`}
         </h4>
       )}
       {children ? (
         <p
-          data-aos-delay={300}
-          data-aos={"fade-up"}
-          data-aos-duration={700}
-          className="mt-30"
         >
           {children}
         </p>
--- src/elements/FeatureBox/FeatureBoxThree.js
+++ src/elements/FeatureBox/FeatureBoxThree.js
@@ -4,9 +4,6 @@ import Icofont from "react-icofont";
 const FeatureBoxThree = ({ index, icon, title, text }) => (
   <div
     className="col-md-3 feature-box text-center col-sm-6"
-    data-aos-delay={`${index}00`}
-    data-aos={"fade-up"}
-    data-aos-duration={1000}
   >
     <Icofont
       icon={icon}
